fa862b98579fde5ab165feb9743a2a805f4f43d7
Andrei Stoica <andrei@stoica.dev>
2025-01-28 19:19:48 +0200

    Create src

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,4 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./src/global.css" });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./global.css"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
