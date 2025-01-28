3acb69b66fcd5c4d020e39f66c7538c45eb39219
Andrei Stoica <andrei@stoica.dev>
2025-01-28 19:18:22 +0200

    setup nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx", "./global.css"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
